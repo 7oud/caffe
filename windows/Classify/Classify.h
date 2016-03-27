8132262475d0aae70f948c7f93f5ae2c88145058
7oud <da0909@163.com>
2016-03-27 13:03:00 +0800

    Caller dll - 1

---
+++ windows/Classify/Classify.h
@@ -0,0 +1,52 @@
+#ifndef __CLASSIFY_H__
+#define __CLASSIFY_H__
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <algorithm>
+#include <iosfwd>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+#include <iostream>
+
+#include "caffe/caffe.hpp"
+
+using namespace cv;
+using namespace caffe;
+using std::string;
+
+typedef std::pair<std::string, float> Prediction;
+
+class Classifier {
+ public:
+  Classifier(const string& model_file,
+             const string& trained_file,
+             const string& mean_file,
+             const string& label_file);
+
+  std::vector<Prediction> Classify(const cv::Mat& img, int N = 5);
+
+ private:
+  void SetMean(const string& mean_file);
+
+  std::vector<float> Predict(const cv::Mat& img);
+
+  void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+
+  void Preprocess(const cv::Mat& img,
+                  std::vector<cv::Mat>* input_channels);
+
+ private:
+  shared_ptr<Net<float> > net_;
+  cv::Size input_geometry_;
+  int num_channels_;
+  cv::Mat mean_;
+  std::vector<string> labels_;
+};
+
+
+#endif
